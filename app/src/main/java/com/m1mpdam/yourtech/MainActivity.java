0d466d0d50494ede88530aa158d6d09b1c65daeb
karimbouzid <kaarimbouzid@gmail.com>
2020-12-16 23:30:07 +0100

    List - RecyclerView

--- app/src/main/java/com/m1mpdam/yourtech/MainActivity.java
+++ app/src/main/java/com/m1mpdam/yourtech/MainActivity.java
@@ -1,14 +1,28 @@
 package com.m1mpdam.yourtech;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    String s1[], s2[];
+    int images[] = {R.drawable.mac, R.drawable.msig63, R.drawable.hp15, R.drawable.hp2000, R.drawable.dellinspiron5050};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rView);
+        s1 = getResources().getStringArray(R.array.laptops);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
