e4ee74580416df2c73b31016c1fd8f6ec5daa86b
karimbouzid <kaarimbouzid@gmail.com>
2020-12-16 23:59:33 +0100

    one item Details Activity

--- app/src/main/java/com/m1mpdam/yourtech/MyAdapter.java
+++ app/src/main/java/com/m1mpdam/yourtech/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.m1mpdam.yourtech;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
@@ -36,6 +38,17 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         holder.myText1.setText(data1[position]);
         holder.myText2.setText(data2[position]);
         holder.myImage.setImageResource(images[position]);
+
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, detailsActivity.class);
+                intent.putExtra("data1",data1[position]);
+                intent.putExtra("data2",data2[position]);
+                intent.putExtra("myImage",images[position]);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -46,11 +59,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView myText1, myText2;
         ImageView myImage;
+        ConstraintLayout mainLayout;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             myText1 = itemView.findViewById(R.id.laptop_txt);
             myText2 = itemView.findViewById(R.id.description_txt);
             myImage = itemView.findViewById(R.id.myImageView);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
         }
     }
 }
